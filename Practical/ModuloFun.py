45150150736c9929f4b4a3c8bd8b7d0ba8d785e6
Abhang Paturkar <155937375+AbhangPaturkar2023BIT508@users.noreply.github.com>
2024-08-15 23:56:30 +0530

    .\ModuloFun.py

--- Practical/ModuloFun.py
+++ Practical/ModuloFun.py
@@ -1,6 +1,9 @@
 def ModuloFun(numerator, denominator):
-    quotient = numerator // denominator
-    mul = quotient * denominator;
-    return int(numerator - mul);
+    if denominator == 0:
+        raise ValueError("Denominator cannot be zero");
+    return numerator - (numerator // denominator) * denominator;
+    # quotient = numerator // denominator
+    # mul = quotient * denominator;
+    # return int(numerator - mul);
 
-print(ModuloFun(-51,-10))
+print(ModuloFun(51,10))
